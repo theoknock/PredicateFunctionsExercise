a41d947b1998817df03990bbf7cbd40c95cdb00b
James Alan Bush <jamesalanbush1972@gmail.com>
2022-04-10 17:09:41 -0400

    Turning the former 'bits' block into a byte-processing template for state machines and any other device that uses bit manipulation algorithms

--- PredicateFunctionsExercise/ViewController.h
+++ PredicateFunctionsExercise/ViewController.h
@@ -6,6 +6,132 @@
 //
 
 #import <UIKit/UIKit.h>
+@import simd;
+
+// TO-DO: The arrays should be external to their closing blocks for vector operations
+static const unsigned int (^guarded_bool_expr)(const unsigned int) = ^ (const unsigned int boolean_operation_index) {
+    return ^ (const unsigned int boolean_operation[16]) {
+        return boolean_operation[boolean_operation_index];
+    }((const unsigned int[16]){
+        (0 << 0 | 0 << 1 | 0 << 2 | 0 << 3), (0 << 0 | 0 << 1 | 0 << 2 | 1 << 3), (0 << 0 | 0 << 1 | 1 << 2 | 0 << 3), (0 << 0 | 0 << 1 | 1 << 2 | 1 << 3),
+        (0 << 0 | 1 << 1 | 0 << 2 | 0 << 3), (0 << 0 | 1 << 1 | 0 << 2 | 1 << 3), (0 << 0 | 1 << 1 | 1 << 2 | 0 << 3), (0 << 0 | 1 << 1 | 1 << 2 | 1 << 3),
+        (1 << 0 | 0 << 1 | 0 << 2 | 0 << 3), (1 << 0 | 0 << 1 | 0 << 2 | 1 << 3), (1 << 0 | 0 << 1 | 1 << 2 | 0 << 3), (1 << 0 | 0 << 1 | 1 << 2 | 1 << 3),
+        (1 << 0 | 1 << 1 | 0 << 2 | 0 << 3), (1 << 0 | 1 << 1 | 0 << 2 | 1 << 3), (1 << 0 | 1 << 1 | 1 << 2 | 0 << 3), (1 << 0 | 1 << 1 | 1 << 2 | 1 << 3)});
+};
+
+static const unsigned int (^conditional_bool_expr)(const unsigned int, const unsigned int, const unsigned int) = ^ (const unsigned int bool_exp_a, const unsigned int bool_exp_b, const unsigned int boolean_operation_index) {
+    return ((const unsigned int[16]){
+        bool_exp_a & ~bool_exp_a,
+        ~bool_exp_a & ~bool_exp_b,
+        ~bool_exp_a & bool_exp_b,
+        ~bool_exp_a,
+        bool_exp_a & ~bool_exp_b,
+        ~bool_exp_b,
+        (bool_exp_a | bool_exp_b) & ~(bool_exp_a & bool_exp_b),
+        ~(bool_exp_a & bool_exp_b),
+        (bool_exp_a & bool_exp_b),
+        (bool_exp_a & bool_exp_b) | (~(bool_exp_a) & ~(bool_exp_b)),
+        bool_exp_b,
+        bool_exp_a | ~(bool_exp_b),
+        bool_exp_a | bool_exp_b,
+        bool_exp_a | ~(bool_exp_a)})[boolean_operation_index];
+};
+
+void mask_on_position(unsigned int * byte, unsigned char position) {
+    * byte = (* byte) | (1 << position);
+}
+
+void mask_off_position(unsigned int * byte, unsigned char position) {
+    unsigned long mask = 1 << position;
+    * byte = (* byte) & ~mask;
+}
+
+unsigned long modifyBit(unsigned int byte, unsigned char position, unsigned int state) {
+    unsigned long mask = 1 << position;
+    return ( byte & ~mask) | (-state & mask);
+}
+
+int flipBit(unsigned int byte, unsigned char position) {
+    int mask = 1 << position;
+    return  byte ^ mask;
+}
+
+unsigned char isBitSet(unsigned char * bit, unsigned char position) {
+    *bit >>= position;
+    return !!(*bit & 1);
+}
+
+int extractBit(unsigned int byte, unsigned int length, unsigned int position) {
+    return (((1 << length) - 1) & (byte >> (position - 1)));
+}
+
+// To-Do:
+//        (typeof(byte_expression) takes typeof(filter) returning typeof(byte_evaluation)) takes (typeof(bit_expression) returns typeof(bit_evaluation))
+//        (typeof(byte_operation) -------------------------------------------------------)       (typeof(bit_operation) -------------------------------)
+//        (typeof(bit_vector_operation) ---------------------------------------------------------------------------------------------------------------)
+
+typedef unsigned char bit;
+    typedef typeof(bit) bit_state;
+    typedef typeof(bit) bit_mask;
+typedef typeof(bit *) byte;
+    typedef typeof(byte) byte_state;
+    typedef typeof(byte) byte_mask;
+
+typedef typeof(size_t) operation_parameter;
+    typedef typeof(operation_parameter) length;
+    typedef typeof(operation_parameter) position;
+
+typedef typeof(bit(^)(bit, bit_mask, bit_state, position, length)) expression_evaluator;
+typedef typeof(bit(^)(expression_evaluator)) evaluation;
+typedef typeof(expression_evaluator(^)(byte, byte_mask, byte_state, position, length)) expression;
+typedef typeof(evaluation(^)(expression)) evaluate_expression;
+
+
+// byte --->
+//           expression ---> expression_evaluator
+//                                                ---> evaluate_expression ---> evaluation
+//                                                                                         ---> bit
+// bit(evaluation(evaluate_expression(expression(byte))))
+
+typedef typeof(bit(^)(void)) bit_evaluator;
+typedef typeof(bit(^)(bit_evaluator)) bit_evaluation;
+typedef typeof(bit_evaluation(^)(bit_evaluator)) bit_expression;
+    typedef typeof(bit_evaluation(^)(bit_expression)) bit_operation;
+
+typedef typeof(byte(^)(unsigned char * bit, unsigned char position, unsigned char length, unsigned char * mask)) byte_expression; // accumulates typeof(bit_evaluation)
+typedef typeof(byte(^)(byte_expression)) byte_evaluation;
+// typeof(byte_operation) accumulates typeof(bit) from typeof(byte), starting at typeof(position) and ending with typeof(length) of typeof(byte)
+    typedef typeof(byte(^)(byte_expression)) byte_operation;
+
+typedef typeof(unsigned int(^)() bit_vector_operation;
+
+unsigned int (^(^bit_vector_operation)(unsigned int, unsigned char, unsigned char, unsigned int))(unsigned char *, unsigned char, unsigned char, unsigned char *)  = ^ (unsigned int byte, unsigned char position, unsigned char length, unsigned int byte_mask) {
+    return ^ (unsigned int(^bit_expression)(unsigned char *, unsigned char, unsigned char, unsigned char *)) {
+        return (^ (unsigned int(^byte_operation)(void)) {
+            /*
+             Map (iterator) and Reduce (Accumulator) filter space
+             */
+            
+            for (unsigned char index   = position; index < length; index++) {
+                unsigned char bit      = (unsigned char)(byte >> index);
+                unsigned char bit_mask = (unsigned char)(byte_mask >> index);
+                printf("%u", bit_operation(&bit, index, length, &bit_mask));
+            }; printf("\n");
+            
+
+            return ^ (unsigned int(^byte_operation)(void)) {
+                return byte_operation;
+            };
+        }(^ unsigned int {
+            return guarded_bool_expr(4);
+        })(^ unsigned int {
+            return guarded_bool_expr(4);
+        }));
+    }(^ unsigned int (unsigned char * bit, unsigned char position, unsigned char length, unsigned char * mask) { // bit_expression
+        return (unsigned int)(isBitSet(bit, position)); // typeof(bit_evaluation)
+    });
+};
+
 
 @interface ViewController : UIViewController
 
--- PredicateFunctionsExercise/ViewController.m
+++ PredicateFunctionsExercise/ViewController.m
@@ -8,6 +8,148 @@
 #import "ViewController.h"
 @import simd;
 
+/*
+ Functors invoked with an argument supplied by the caller
+ */
+typedef const float (^ const __strong float_blk)(const float);
+float_blk b = ^ const float (const float f) {
+    return f;
+};
+
+typedef const void * (^ const __strong float_blk_t)(float_blk);
+float_blk_t b_ptr = ^ (float_blk b) {
+    return Block_copy((const void *)CFBridgingRetain(b));
+};
+
+// There should be no external-facing return type; this is an inner block to an outer void(^)(const void *), both of which are implemented (returned) by the callee
+typedef float_blk (^ const __strong functor)(const void *);
+functor f = ^ (const void * float_blk_ptr) {
+    return (float_blk)CFBridgingRelease((__bridge CFTypeRef _Nullable)((__bridge float_blk)(float_blk_ptr)));
+};
+
+/*
+ Functors invoked with an argument supplied by the callee (returns the uninvoked functor, which can be invoked with 'void' instead of the argument required by the functor)
+ */
+
+/*
+ 
+- [ ] float (^)(void)  // callee supplies float parameter (evaluated during initialization: float(^(^))(float))(void)
+- [ ] float (^)(void) // callee supplies the float parameter (evaluated during invocation: float(^(^(^))(float))(float))(void) - like a composition
+- [ ] float //
+^ float {
+    ^ (float f) {
+        return f;
+    }(f);
+}();
+ 
+ */
+
+typedef const float (^ const __strong float_to_float_blk)(const float);
+float_to_float_blk fb = ^ const float (const float f) {
+    return f;
+};
+//typedef const float(^(^ const __strong float_using_float_blk))(const float))(void);
+//float_using_float_blk ffb = ^ (const float f) {
+//    return ^ (const float_blk fb) {
+//        return ^ (const float f) {
+//            return ^ const float {
+//                return fb(f);
+//            };
+//        };
+//    }(b)(f);
+//};
+
+// Passes block with its parameter and evaluates it when invoked by the caller (lazy evaluation?)
+typedef const float (^float_blk_)(void);
+typedef const float_blk_ (^(^ const __strong float_using_float_blk_)(float_to_float_blk))(const float);
+float_using_float_blk_ ffb_ = ^ (float_to_float_blk fb) {
+    return ^ (const float(^const __strong fb)(const float)) {
+        return ^ (const float f) {
+            return ^ const float {
+                return ^ (const float f) {
+                    return f;
+                }(f);
+            };
+        };
+    }(fb);
+};
+
+
+
+
+
+//typedef const float (^(^blk_pack)(const float))(void);
+//blk_pack fb = ^ (const float f_) {
+//    return ^ (const blk b_) {
+//        return ^ (const float f) {
+//            return ^ const float {
+//                return b_(f);
+//            };
+//        };
+//    }(b)(f_);
+//};
+
+
+// A block that creates a blk_pack without invoking its inner blk
+// (used when source provides the float parameter, but when the float parameter should be evaluated at the destination)
+// (for example: when one of the state bit vectors should be read only after the block reaches the destination;
+//  right now, blk_pack would use the value of a state bit vector at the time the blk_pack was created)
+//typedef const float (^(^(^blk_pack_)(const float))(const float))(void);
+//blk_pack_ fb = ^ (const float f_) {
+//    return ^ (const blk b_) {
+//        return ^ (const float f) {
+//            return ^ const float {
+//                return b_(f);
+//            };
+//        };
+//    }(b)(f_); // to return only the blk ('b'), omit the second parameter and change the return block to float(float) versus float(void)
+//};
+
+
+//typedef const float (^f_blk)(void);
+//f_blk fb = ^ (const float f) {
+//    return ^ const float {
+//        return f;
+//    };
+//}(b(UIScreen.mainScreen.scale));
+//
+//blk b_ = ^ blk (f_blk fb) {
+//    return ^ const float (const float f) {
+//        return f;
+//    };
+//}(fb);
+
+
+//typedef const void * (^ const __strong blk_pack_t)(blk_pack);
+//blk_pack_t bp_t = ^ (blk_pack bp) {
+//    return Block_copy((const void *)CFBridgingRetain(bp));
+//};
+//
+//typedef blk_pack (^ const __strong functor_pack)(const void *);
+//functor_pack fp = ^ (const void * functor_pack_t) {
+//    return (blk_pack)CFBridgingRelease((__bridge CFTypeRef _Nullable)((__bridge blk_pack)(functor_pack_t)));
+//};
+
+
+
+
+
+// To-Do: Return a generic type and typecast in the assignment/initialization
+
+
+
+/*
+ Example of packing the parameter with the block so that the parameter can be supplied at the origin (prior to destination):
+ 
+    const void * blk_ptr = ((__bridge const float(^ const __strong)(float))(float_blk_t(CFBridgingRelease((__bridge CFTypeRef _Nullable)(float_blk)))))((float)UIScreen.mainScreen.scale));
+
+ Example of packing just the block so that the parameter can be supplied at its destination:
+ 
+    typeof(float (^)(float)) float_blk_from_ptr = functor(float_blk_t((typeof(float (^)(float)))float_blk));
+    float float_value = float_blk_from_ptr(UIScreen.mainScreen.scale);
+    printf("\t\t%.2f", float_value);
+*/
+
 typedef int (^boolean_expression)(void);
 typedef int (^guarded_boolean_expression)(void);
 typedef int (^ const (*guarded_boolean_expression_t))(void);
@@ -16,29 +158,33 @@ typedef int (^ const (^ const (*conditional_boolean_expression_t))(guarded_boole
 typedef int (^predicate)(void);
 typedef int (^ const (*predicate_t))(void);
 
-static int c = 0;
-static int flag = 1;
+static unsigned int c = 0;
+static unsigned int flag = 1;
 guarded_boolean_expression flag_conditional = ^ int { return flag; };
-
 
 conditional_boolean_expression isEven = ^ (guarded_boolean_expression_t _Nullable x) { return ^ int { int result = (*x)() % 2 == 0; printf("\n%d %s (%d)\n", flag, (result) ? "is even"     : "is not even",     result); return result; }; };
 conditional_boolean_expression isOdd  = ^ (guarded_boolean_expression_t _Nullable x) { return ^ int { int result = (*x)() % 2 == 1; printf("\n%d %s (%d)\n", flag, (result) ? "is odd"      : "is not odd",      result); return result; }; };
 conditional_boolean_expression isPos  = ^ (guarded_boolean_expression_t _Nullable x) { return ^ int { int result = (*x)() > 0;      printf("\n%d %s (%d)\n", flag, (result) ? "is positive" : "is not positive", result); return result; }; };
 conditional_boolean_expression isNeg  = ^ (guarded_boolean_expression_t _Nullable x) { return ^ int { int result = (*x)() < 0;      printf("\n%d %s (%d)\n", flag, (result) ? "is negative" : "is not negative", result); return result; }; };
 
-static void (^evaluate_predicate)(predicate_t) = ^ (predicate_t p) {
-    printf("\nresult = %d (%d invocations)\n", (*p)(), c);
+
+
+static void (^evaluate_predicate)(unsigned int) = ^ (unsigned int index) {
+    printf("%d %d", (1UL << 0 | 1UL << 1^ | 1UL << 2 | 1UL << 3), ~(1UL << 0 | 1UL << 1 | 1UL << 2 | 1UL << 3));
+    bits(conditional_bool_expr((1UL << 0 | 1UL << 1 | 1UL << 2 | 1UL << 3), (1UL << 0 | 1UL << 1 | 1UL << 2 | 1UL << 3), index));
 };
 
-static int (^(^(^(^g)(__strong conditional_boolean_expression, __strong conditional_boolean_expression, guarded_boolean_expression_t))(int (^__strong)(conditional_boolean_expression_t, conditional_boolean_expression_t, guarded_boolean_expression_t)))(int (^__strong)(int)))(void) =
-^ (conditional_boolean_expression boolean_conditional_a, conditional_boolean_expression boolean_conditional_b, guarded_boolean_expression_t guarded_conditional) {
-    return ^ (int (^bitwise_operation)(conditional_boolean_expression_t, conditional_boolean_expression_t, guarded_boolean_expression_t)) {
-        return ^ (int(^block)(int)) {
-            return ^ (int boolean_expression) {
-                return ^ int {
-                    return boolean_expression && block(boolean_expression);
+static conditional_boolean_expression_t (^(^(^(^g)(__strong conditional_boolean_expression, __strong conditional_boolean_expression, guarded_boolean_expression_t))
+                                                  (conditional_boolean_expression_t (^__strong)(conditional_boolean_expression_t, conditional_boolean_expression_t, guarded_boolean_expression_t)))
+                                                  (conditional_boolean_expression_t (^__strong)(conditional_boolean_expression_t)))(void) =
+^ (conditional_boolean_expression bool_exp_a, conditional_boolean_expression bool_exp_b, guarded_boolean_expression_t guarded_conditional) {
+    return ^ (conditional_boolean_expression_t (^evaluate_boolean_expression)(conditional_boolean_expression_t, conditional_boolean_expression_t, guarded_boolean_expression_t)) {
+        return ^ (conditional_boolean_expression_t(^ _Nullable functor)(conditional_boolean_expression_t)) {
+            return ^ (conditional_boolean_expression_t boolean_expression) {
+                return ^ conditional_boolean_expression_t {
+                    return functor(boolean_expression);
                 };
-            }(bitwise_operation((conditional_boolean_expression_t)&boolean_conditional_a, (conditional_boolean_expression_t)&boolean_conditional_b, guarded_conditional));
+            }(evaluate_boolean_expression((conditional_boolean_expression_t)&bool_exp_a, (conditional_boolean_expression_t)&bool_exp_b, guarded_conditional));
         };
     };
 };
@@ -49,25 +195,111 @@ static int (^(^(^(^g)(__strong conditional_boolean_expression, __strong conditio
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // gbe_a is set to a precalculated and stored result
-    // Invoking gbe_a() merely returns the results without recalculating it
-    guarded_boolean_expression gbe_a = g(isEven, isOdd, &flag_conditional)(^ int (conditional_boolean_expression_t boolean_conditional_a, conditional_boolean_expression_t boolean_conditional_b, guarded_boolean_expression_t guarded_conditional) {
-        return ((*boolean_conditional_a)(guarded_conditional)() & (*guarded_conditional)()) && (*boolean_conditional_b)(guarded_conditional);
-    })(^ int (int i) {
-        printf("\t\tgbe_a()\n");
-        return i;
-    });
+    for (int i = 0; i < 16; i++) evaluate_predicate(i);
     
-    guarded_boolean_expression gbe_b = g(isPos, isNeg, &flag_conditional)(^ int (conditional_boolean_expression_t boolean_conditional_a, conditional_boolean_expression_t boolean_conditional_b, guarded_boolean_expression_t guarded_conditional) {
-        return ((*boolean_conditional_a)(guarded_conditional)() & (*guarded_conditional)()) && (*boolean_conditional_b)(guarded_conditional);
-    })(^ int (int i) {
-        printf("\t\tgbe_b()\n");
-        return i;
-    });
+    typedef const float (^ const __strong float_exp)(const float);
+    float_exp f_exp = ^ const float (const float f) {
+        return f;
+    };
+    float fe_f = f_exp(1.0);
     
-    printf("gbe_a() == %d\n", gbe_a());
-    printf("gbe_b() == %d\n", gbe_b());
-    printf("gbe_a() & gbe_b() == %d\n", gbe_a() & gbe_b());
+    typedef const float (^ const __strong float_eval)(void);
+    float_eval f_eval = ^ (float_exp fb) {
+        return ^ (const float f) {
+            return ^ (const float f) {
+                return ^ (const float f) {
+                    return ^ const float {
+                        return fb(f);
+                    };
+                }(f);
+            }(f);
+        };
+    }(f_exp)(1.0);
+    float_eval * f_eval_t = &f_eval;
+    float f_eval_f = (*f_eval_t)();
+    
+    
+
+//    blk functor_f = f(b_ptr(b));
+//    float float_value = functor_f(UIScreen.mainScreen.scale);
+//    printf("\t\t%.2f", float_value);
+//
+//    printf("\nfloat_blk_literal == %.1f\n", ^ (const float f) {
+//        return ^ const float {
+//            return f;
+//        };
+//    }(b(UIScreen.mainScreen.scale))());
+//
+//    float f = ^ (const float f) {
+//        return ^ const float {
+//            return f;
+//        };
+//    }(b(UIScreen.mainScreen.scale))();
+//
+//    typedef const float (^f_blk)(void);
+//    f_blk fb = ^ (const float f) {
+//        return ^ const float {
+//            return f;
+//        };
+//    }(b(UIScreen.mainScreen.scale));
+//
+//    blk b_ = ^ blk (f_blk fb) {
+//        return ^ const float (const float f) {
+//            return f;
+//        };
+//    }(fb);
+
+
+//    const void * blk_ptr = (const void *)(b_ptr(CFBridgingRelease((__bridge CFTypeRef _Nullable)(b))))(1.0);//((float)UIScreen.mainScreen.scale);
+
+    
+    // PARAMETER(S)
+        // (float_blk_t((const typeof(const float (^)(const float)))float_blk));
+    // RETURN
+    //     ((__bridge const float(^ const __strong)(float))(float_blk_t(CFBridgingRelease((__bridge CFTypeRef _Nullable)(float_blk)))))((float)UIScreen.mainScreen.scale);
+
+    // PARAMETER(S)
+//         (float_blk_t((const typeof(const float (^)(const float)))float_blk));
+    // RETURN (Example of packing the parameter with the block so that the parameter can be supplied at the origin (prior to destination)
+//    printf("\t\t%.2f\n", ((__bridge const float(^ const __strong)(float))(float_blk_t(CFBridgingRelease((__bridge CFTypeRef _Nullable)(float_blk)))))((float)UIScreen.mainScreen.scale));
+    
+    // RETURN = PARAMETER(S)
+    // Example of packing just the block so that the parameter can be supplied at its destination
+//    typeof(float (^)(float)) float_blk_from_ptr = functor(float_blk_t((typeof(float (^)(float)))float_blk));
+//    float float_value = float_blk_from_ptr(UIScreen.mainScreen.scale);
+//    printf("\t\t%.2f", float_value);
+
+//    conditional_boolean_expression_t gbe_x = g(isEven, isOdd, &flag_conditional)
+//    (^conditional_boolean_expression_t (conditional_boolean_expression_t bool_exp_a, conditional_boolean_expression_t bool_exp_b, guarded_boolean_expression_t guarded_conditional) {
+//        return bool_exp_a;
+//    })
+//    (^ conditional_boolean_expression_t (conditional_boolean_expression_t b) {
+//        return ^ conditional_boolean_expression_t {
+//            return b;
+//        };
+//    });
+    
+    
+//    conditional_boolean_expression_t gbe_a = g(isEven, isOdd, &flag_conditional)
+//    (^ conditional_boolean_expression_t (conditional_boolean_expression_t bool_exp_a, conditional_boolean_expression_t bool_exp_b, guarded_boolean_expression_t guarded_conditional) {
+//        return ((*bool_exp_a)(guarded_conditional)() & (*guarded_conditional)()) && (*bool_exp_b)(guarded_conditional);
+//    })(^ conditional_boolean_expression_t (conditional_boolean_expression_t boolean_evaluation) {
+////        return Block_copy((conditional_boolean_expression_t)CFBridgingRetain(^ conditional_boolean_expression_t {
+////            printf("\t\tgbe_a() == %d\n", boolean_evaluation);
+////            return boolean_evaluation;
+////        }));
+//    });
+    
+//    guarded_boolean_expression gbe_b = g(isPos, isNeg, &flag_conditional)(^ int (conditional_boolean_expression_t bool_exp_a, conditional_boolean_expression_t bool_exp_b, guarded_boolean_expression_t guarded_conditional) {
+//        return ((*bool_exp_a)(guarded_conditional)() & (*guarded_conditional)()) && (*bool_exp_b)(guarded_conditional);
+//    })(^ int (int i) {
+//        printf("\t\tgbe_b()\n");
+//        return i;
+//    });
+    
+//    printf("gbe_a() == %d\n", gbe_a());
+//    printf("gbe_b() == %d\n", gbe_b());
+//    printf("gbe_a() & gbe_b() == %d\n", gbe_a() & gbe_b());
     
     // Returning a variety of bitwise operations with randomly chosen operands and operators
     // to demonstrate that simd vectors can be used combining multiple bitwise operations into a single operation
